34f78e16f0638b499c5dd1036013eadf07f79ad1
Smittward <psmith9263@gmail.com>
2017-10-27 19:45:32 -0400

    update queue

--- Queue.hs
+++ Queue.hs
@@ -1,25 +1,41 @@
--- :script C:\Users\Patrick\HaskellProjects\Queue.txt
+-- :script C:\Users\Patrick\HaskellProjects\Queue.hs
 :! cls
 
 
-append(value, queue) = queue ++ [value]
-
-:{
-enqueue(queue, item) =
-  do putStrLn "h"
-  let result = append(item, queue) in result
-
-:}
-
-
+append(list, value) = list ++ [value]
+enqueue(queue, item) = append(queue, item)
 dequeue(queue) = (head queue,tail queue)
 
-
-
-emptyQueue = []
-
-q1 = enqueue(emptyQueue, 1)
-
+q1 = []
 q2 = enqueue(q1, 100)
-
 q3 = enqueue(q2, 200)
+q4 = enqueue(q3, 300)
+-- O(1)?
+-- monads
+:{
+largeQueue(queue, list) =
+  let
+  currentItem = head list
+  nextList = tail list
+  in
+  if length list > 0
+    then
+      do
+      --(show currentItem)
+      -- ++ " "
+      largeQueue(enqueue(queue, currentItem),nextList)
+      else queue
+:}
+
+largeQueue2(queue, list) = queue ++ list
+
+example = [1..100000000000000]
+a = dequeue([1..10000000000000])
+example2 = dequeue(example)
+a = enqueue(example, 1000)
+b = largeQueue(example, [1..10000000])
+c = largeQueue2(example,[1..10000000])
+
+
+putStrLn ""
+putStrLn "Process finished with exit code 0"
