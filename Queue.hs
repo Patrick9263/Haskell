5085842d580576a333206d3bf3e5ab234b5e76d4
Patrick <patrick.smith@uconn.edu>
2017-10-29 15:29:21 -0400

    work on queue

--- Queue.hs
+++ Queue.hs
@@ -20,18 +20,36 @@ largeQueue(queue, list) =
   nextList = tail list
   in
   if length list > 0
-    then do
-      putStr (show currentItem);
-      putStr " ";
+    then --do
+      --putStr (show currentItem);
+      --putStr " ";
       largeQueue(enqueue(queue, currentItem),nextList)
-      else return ""
+      else queue
 :}
 
 example = [1..10000000000000000000000000000000000000000000000000000000000000000]
 a = dequeue([1..10000000000000000000000000000000000000000000000000000000000000])
 b = enqueue(example,10000000000000000000000000000000000000000000000000000000000)
 c = largeQueue(example,[1..100000000000000000000000000000000000000000000000000])
-largeQueue([1..500],[1..100])
+d = largeQueue([1..50],[1..100000])
+
+test1 = [1..10000000000000000000000000000000000000000000000000000000]
+test2 = fst (dequeue test1)
+test3 = enqueue(test1,10000000000000000000000)
+test4 = fst (dequeue test3)
+
+
+
+
+
+
+:{
+f :: Int -> Int
+f 0 = 1
+f 1 = 5
+f 2 = 2
+f _ = -1
+:}
 
 
 
