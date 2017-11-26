a13912afad01f7866d35f691a4f53e881e7e60a1
Patrick <psmith9263@gmail.com>
2017-11-25 23:29:43 -0500

    remove extra parenthesis

--- LinkedList.hs
+++ LinkedList.hs
@@ -3,13 +3,13 @@ import System.Process
 clear = system "cls"
 clear
 
-addFirst(value, linkList) = ((), value : snd linkList)
-removeFirst(linkList) = (head (snd linkList), tail (snd linkList))
-removeLast(linkList) = (last (snd linkList), init (snd linkList))
-addLast(value, linkList) = ((), snd linkList ++ [value])
+addFirst(value, linkList) = value : linkList
+removeFirst(linkList) = (head linkList, tail linkList)
+removeLast(linkList) = (last linkList, init linkList)
+addLast(value, linkList) = linkList ++ [value]
 
 
-link = ((), [])
+link = []
 link2 = addFirst(1, link)
 link3 = addFirst(2, link2)
 link4 = addFirst(3, link3)
@@ -18,5 +18,5 @@ link5 = addFirst(4, link4)
 link6 = addLast(0, link5)
 link7 = addLast(-1, link6)
 
-link8 = removeFirst(link7)
+link8 = snd(removeFirst(link7))
 addFirst(5, link8)
